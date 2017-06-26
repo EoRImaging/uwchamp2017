b1777ea878e6566ed2d499efc973b175909844ae
dcunningham725 <devin.cunningham@sjsu.edu>
2017-06-26 15:06:32 -0700

    Add files via upload

--- sourceplot.py
+++ sourceplot.py
@@ -5,42 +5,43 @@ import scipy
 from scipy import io
 import glob
 
-def readandplot(location):
-    '''Description'''
+def collector(path):
+    '''Collects all IDL save data from a given path and stores each file as an element in a list.'''
         
-        ###==================================================================###
-        ###------------------------------------------------------------------###
-        ###--------------Collecting All Data Files from Folder---------------###
-        ###------------------------------------------------------------------###
-        ###==================================================================###
+    ###==================================================================###
+    ###------------------------------------------------------------------###
+    ###--------------Collecting All Data Files from Folder---------------###
+    ###------------------------------------------------------------------###
+    ###==================================================================###
     
-    data = []
-    all_files = glob.glob(location)
-    for i in range(len(all_files)):
-        data.append(scipy.io.readsav(all_files[i],python_dict=True))
-        
-        ###==================================================================###
-        ###------------------------------------------------------------------###
-        ###--------------Separating Point and Extended Sources---------------###
-        ###------------------------------------------------------------------###
-        ###==================================================================###
-        
-        ###==================================================================###
-        ###----------------Initializing comprehensive lists------------------###
-        ###==================================================================###
+    filenames = glob.glob(path)
+    
+    data = [[scipy.io.readsav(filenames[i],python_dict=True) for i in range(len(filenames))],filenames]
+    return data
+
+def separator(data):
+    '''Compiles data into separate lists of extended and point sources'''
+    ###==================================================================###
+    ###------------------------------------------------------------------###
+    ###--------------Separating Point and Extended Sources---------------###
+    ###------------------------------------------------------------------###
+    ###==================================================================###
+
+    ###==================================================================###
+    ###----------------Initializing comprehensive lists------------------###
+    ###==================================================================###
     
     point_data = []
     extended_data = []
-    
     extended_X = []
     extended_Y = []
     extended_FREQ = []
     extended_I = []
     
-    for i in range(len(all_files)):
+    for i in range(len(data[0])):
         
         ###==================================================================###
-        ###--------------------Initializing nested lists---------------------###
+        ###---------------Initializing nested separated lists----------------###
         ###==================================================================###
         
         extended_data_nest = []
@@ -51,17 +52,17 @@ def readandplot(location):
         extended_I_nest = []
         
         ###==================================================================###
-        ###-----------------------Creating nested lists----------------------###
+        ###------------------Creating nested separated lists-----------------###
         ###==================================================================###
         
-        for j in range(len(data[i]['source_array'])):
-            if data[i]['source_array'][j][-2] is None:
-                point_data_nest.append(data[i]['source_array'][j])
+        for j in range(len(data[0][i]['source_array'])):
+            if data[0][i]['source_array'][j][-2] is None:
+                point_data_nest.append(data[0][i]['source_array'][j])
             else:
-                extended_data_nest.append(data[i]['source_array'][j])
+                extended_data_nest.append(data[0][i]['source_array'][j])
         
         ###==================================================================###
-        ###------------------Creating fully separated lists------------------###
+        ###--------------Creating comprehensive separated lists--------------###
         ###==================================================================###
         
         point_data.append(point_data_nest)
@@ -85,13 +86,24 @@ def readandplot(location):
         extended_Y.append(extended_Y_nest)
         extended_FREQ.append(extended_FREQ_nest)
         extended_I.append(extended_I_nest)
-        
-        ###==================================================================###
-        ###------------------------------------------------------------------###
-        ###------------------Plotting All Extended Sources-------------------###
-        ###------------------------------------------------------------------###
-        ###==================================================================###
-        
+
+    return extended_data
+    #return point_data
+    #return extended_X
+    #return extended_Y
+    #return extended_FREQ
+    #return extended_I
+
+def plotall(data):
+    ###==================================================================###
+    ###------------------------------------------------------------------###
+    ###------------------Plotting All Extended Sources-------------------###
+    ###------------------------------------------------------------------###
+    ###==================================================================###
+    
+    separator(data)
+    
+    for i  in range(len(data)):
         plt.figure(figsize=(18,15))
         plt.suptitle('{}'.format(all_files[i]),fontsize=20)
         
@@ -118,3 +130,29 @@ def readandplot(location):
         
         plt.show()
 
+def plotext(data,minI):
+    
+    extended_data = separator(data)
+    indi_I = []
+    indi_I_nest = []
+    indi_I_nest2 = []
+    
+    for i in range(len(data[0])):
+        for j in range(len(extended_data[i][0]['EXTEND'])):
+            for k in range(len(extended_data[i][j]['EXTEND']['FLUX'])):
+                indi_I_nest2.append(extended_data[i][j]['EXTEND']['FLUX'][k]['I'][0])
+            indi_I_nest.append(indi_I_nest2)
+            indi_I_nest2 = []
+        indi_I.append(indi_I_nest)
+        indi_I_nest = []
+
+    for i in range(len(data[0])):
+        for j in range(len(extended_data[i][j]['EXTEND'])):
+            if max(indi_I[i][j]) > minI:
+                plt.figure()
+                plt.scatter(extended_data[i][j]['EXTEND']['RA'],extended_data[i][j]['EXTEND']['DEC'],s=50,c=indi_I[i][j])
+                plt.colorbar(label='Janskies')
+                plt.figtext(0, -.15, "ObsID: {}\nTotal Flux: {}\nMean RA: {}\nMean Dec: {}".format(data[1][i],np.sum(indi_I[i][j]),extended_data[i][j]['RA'],extended_data[i][j]['DEC']))
+                plt.xlabel('RA')
+                plt.ylabel('DEC')
+    return plt.show()
