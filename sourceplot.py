efb4f9cddf92796c1ff35c9bcd3d0791fcd90499
Nick Tasker <ntasker125@gmail.com>
2017-06-28 13:53:10 -0700

    filedata is a module i made, the others are just updates i needed to commit

--- sourceplot.py
+++ sourceplot.py
@@ -7,132 +7,31 @@ import glob
 
 def collector(path):
     '''Collects all IDL save data from a given path and stores each file as an element in a list.'''
-        
-    ###==================================================================###
-    ###------------------------------------------------------------------###
-    ###--------------Collecting All Data Files from Folder---------------###
-    ###------------------------------------------------------------------###
-    ###==================================================================###
     
     filenames = glob.glob(path)
     
-    data = [[scipy.io.readsav(filenames[i],python_dict=True) for i in range(len(filenames))],filenames]
+    data = {'data':[scipy.io.readsav(filenames[i],python_dict=True) \
+            for i in range(len(filenames))],'filenames':filenames}
     return data
 
 def separator(data):
     '''Compiles data into separate lists of extended and point sources'''
-    ###==================================================================###
-    ###------------------------------------------------------------------###
-    ###--------------Separating Point and Extended Sources---------------###
-    ###------------------------------------------------------------------###
-    ###==================================================================###
 
-    ###==================================================================###
-    ###----------------Initializing comprehensive lists------------------###
-    ###==================================================================###
+    point_data = [data['data'][i]['source_array'][j] \
+    if data['data'][i]['source_array'][j][-2] is None else ''\
+    for i in range(len(data['data'])) \
+    for j in range(len(data['data'][i]['source_array'])) ]
     
-    point_data = []
-    extended_data = []
-    extended_X = []
-    extended_Y = []
-    extended_FREQ = []
-    extended_I = []
-    
-    for i in range(len(data[0])):
-        
-        ###==================================================================###
-        ###---------------Initializing nested separated lists----------------###
-        ###==================================================================###
-        
-        extended_data_nest = []
-        point_data_nest = []
-        extended_X_nest = []
-        extended_Y_nest = []
-        extended_FREQ_nest = []
-        extended_I_nest = []
-        
-        ###==================================================================###
-        ###------------------Creating nested separated lists-----------------###
-        ###==================================================================###
-        
-        for j in range(len(data[0][i]['source_array'])):
-            if data[0][i]['source_array'][j][-2] is None:
-                point_data_nest.append(data[0][i]['source_array'][j])
-            else:
-                extended_data_nest.append(data[0][i]['source_array'][j])
-        
-        ###==================================================================###
-        ###--------------Creating comprehensive separated lists--------------###
-        ###==================================================================###
-        
-        point_data.append(point_data_nest)
-        extended_data.append(extended_data_nest)
-        
-        ###==================================================================###
-        ###------Creating nested lists for parameters of extended sources----###
-        ###==================================================================###
-        
-        for j in range(len(extended_data[i])):
-            extended_X_nest.append(extended_data[i][j]['X'])
-            extended_Y_nest.append(extended_data[i][j]['Y'])
-            extended_FREQ_nest.append(extended_data[i][j]['FREQ'])
-            extended_I_nest.append(extended_data[i][j]['FLUX']['I'][0])
-        
-        ###==================================================================###
-        ###------------Appending nested lists to comprehensive list----------###
-        ###==================================================================###
-        
-        extended_X.append(extended_X_nest)
-        extended_Y.append(extended_Y_nest)
-        extended_FREQ.append(extended_FREQ_nest)
-        extended_I.append(extended_I_nest)
+    extended_data = [data['data'][i]['source_array'][j] \
+    if data['data'][i]['source_array'][j][-2] is not None else ''\
+    for i in range(len(data['data'])) \
+    for j in range(len(data['data'][i]['source_array'])) ]
 
-    return extended_data
-    #return point_data
-    #return extended_X
-    #return extended_Y
-    #return extended_FREQ
-    #return extended_I
-
-def plotall(data):
-    ###==================================================================###
-    ###------------------------------------------------------------------###
-    ###------------------Plotting All Extended Sources-------------------###
-    ###------------------------------------------------------------------###
-    ###==================================================================###
-    
-    separator(data)
-    
-    for i  in range(len(data)):
-        plt.figure(figsize=(18,15))
-        plt.suptitle('{}'.format(all_files[i]),fontsize=20)
-        
-        plt.subplot(3,3,1)
-        plt.scatter(extended_X[i],extended_Y[i],s=10,c=extended_FREQ[i])
-        plt.title('Frequency (Only Extended Sources)')
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.colorbar()
-        
-        plt.subplot(3,3,2)
-        plt.scatter(extended_X[i],extended_Y[i],s=10,c=np.fft.ifft(extended_I[i]),vmax=.3)
-        plt.title('Inverse FT Intensity (Only Extended Sources)')
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.colorbar()
-        
-        plt.subplot(3,3,3)
-        plt.hist2d(extended_X[i],extended_Y[i],bins=100)
-        plt.title('Position (Only Extended Sources)')
-        plt.xlabel('X')
-        plt.ylabel('Y')
-        plt.colorbar()
-        
-        plt.show()
+    return {'extsources':extended_data,'psources':point_data}
 
 def plotext(data,minI):
     
-    extended_data = separator(data)
+    extended_data = separator(data)[0]
     indi_I = []
     indi_I_nest = []
     indi_I_nest2 = []
